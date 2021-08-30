d230c48d4d179d5bf0542f4c80eed1d5fe8d48f4
Jason <j.yuill828@gmail.com>
2021-08-30 00:03:50 -0600

    updates

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 import tkinter as tk
 root = tk.Tk()
 
-root.geometry('300x500')
-root.title("Lable test title")
+root.geometry('300x500+75+200')
+root.title("CAT COUNTER")
+photo = tk.PhotoImage(file='cat.png')
+root.iconphoto(False, photo)
 
-root = tk.Tk()
-
-test = tk.Entry(root, width=50)
-test.pack()
-
-e2 = tk.Entry(root, width=75)
-e2.pack()
-
-bs_label = tk.Label(root, text="this is the bs label")
-bs_label.pack()
-
-def my_click():
-    msg = "Hello " + test.get()
-    test_label = tk.Label(root, text= msg)
-    test_label.pack()
-
-button  = tk.Button(root, text="enter your name", command=my_click)
-button.pack()
 
 root.mainloop()
